f50ca6a89b3ed3129474f5efddde9fe0bdd5fb13
tatede <tatebrewe@icloud.com>
2026-05-02 13:33:35 -0400

    Update auth.ts

--- root/src/auth.ts
+++ root/src/auth.ts
@@ -4,4 +4,9 @@ import Google from "next-auth/providers/google";
 export const { handlers, signIn, signOut, auth } = NextAuth({
   trustHost: true,
   providers: [Google],
+  callbacks: {
+    async redirect({ url, baseUrl }) {
+      return baseUrl;
+    },
+  },
 });
