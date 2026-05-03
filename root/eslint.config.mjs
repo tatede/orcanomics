93ed18d12c6fda8d8bc039b7edff87c04c0369ba
tatede <tatebrewe@icloud.com>
2026-05-02 23:35:56 -0400

    Fix eslint config to handle core web vitals array

--- root/eslint.config.mjs
+++ root/eslint.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextCoreWebVitals from "eslint-config-next/core-web-vitals.js";
+
 export default defineConfig([
-  ...nextCoreWebVitals,
+  ...(Array.isArray(nextCoreWebVitals) ? nextCoreWebVitals : [nextCoreWebVitals]),
   globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
 ]);
